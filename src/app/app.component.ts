4682bd536ae1a175dd0ddd2278e543c7f74fa0e2
martin.dobrev@noser.com <LEviZU87>
2019-01-14 17:26:43 +0200

    changed title, added noser bulgaria logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nb-angular-tutorial';
+  title = 'Martin\'s Angular Tutorial';
 }
